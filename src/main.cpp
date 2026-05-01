2073d78e4d685c060a23f1ae7048aa5914e06468
Javier Bolduc <laaaaaaa104@gmail.com>
2026-05-01 10:30:21 -0400

    basic web page

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,64 @@
 #include <SPIFFS.h>
 #include "display.h"
 
+#define DISP_TX 17
+#define DISP_RX 16
+
 const char* ssid = "ESP_Display";
 const char* password = "12345678";
 
 WebServer server(80);
 
+// -------------------- FILE SERVER --------------------
+void handleFile(String path, String type) {
+  if (!SPIFFS.exists(path)) {
+    server.send(404, "text/plain", "Missing file: " + path);
+    return;
+  }
+
+  File file = SPIFFS.open(path, "r");
+  server.streamFile(file, type);
+  file.close();
+}
+
 // -------------------- SETUP --------------------
 void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, -1, 22, true);
-  SPIFFS.begin(true);
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS mount failed");
+    return;
+  }
+  Serial.println("SPIFFS OK");
+
   WiFi.softAP(ssid, password);
+  Serial.println("WiFi started");
 
   // -------------------- ROUTES --------------------
   server.on("/", HTTP_GET, []() {
-    File f = SPIFFS.open("/index.html", "r");
-    server.streamFile(f, "text/html");
-    f.close();
+    handleFile("/index.html", "text/html");
+  });
+
+  server.on("/app.js", HTTP_GET, []() {
+    handleFile("/app.js", "application/javascript");
+  });
+
+  server.on("/style.css", HTTP_GET, []() {
+    handleFile("/style.css", "text/css");
   });
 
   server.on("/send", HTTP_GET, []() {
-    if (server.hasArg("text"))
-      scrollText(server.arg("text"), 1, 3);
+    if (server.hasArg("text")) {
+      String text = server.arg("text");
+      Serial.println("Got: " + text);
+      scrollText(text, 1, 3);
+    }
     server.send(200, "text/plain", "OK");
   });
 
   server.begin();
+  Serial.println("Server started");
 }
 
 // -------------------- LOOP --------------------
